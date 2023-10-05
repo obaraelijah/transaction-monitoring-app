7f57342bd856aba993d648ad1543a231ae021a64
obaraelijah <elijahobara357@gmail.com>
2023-10-05 15:47:49 +0300

    feat: add common app

--- app/common/models.py
+++ app/common/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class AuditableModel(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
